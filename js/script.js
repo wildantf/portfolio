4ba4f6d283606a1824ad674a2dd244a06669a154
wildantf <wildantammafazchair877@gmail.com>
2019-08-25 20:01:48 +0700

    Ubah Header tiap section

--- js/script.js
+++ js/script.js
@@ -1,48 +1,45 @@
 //event pada saat link pada navbar di klik
-$('.page-scroll').on('click', function(event){
+$('.page-scroll').on('click', function (event) {
     // ambil isi href
     var tujuan = $(this).attr('href');
     // tangkap elemen ybs
-    var elemenTujuan =$(tujuan);
+    var elemenTujuan = $(tujuan);
     //pindahkan scroll
     $('html,body').animate({
         scrollTop: elemenTujuan.offset().top - 50
-    },1250,'easeInOutBack');
-    
-    event.preventDefault(); 
+    }, 1250, 'easeInOutBack');
+
+    event.preventDefault();
 })
 
 // PARALLAX
 //about
-$(window).on('load',function(){
+$(window).on('load', function () {
     console.log('oke');
     $('.p-right').addClass('active');
     $('.p-left').addClass('active');
 });
-$(window).scroll(function(){
+$(window).scroll(function () {
     //jumbotron
-    var wScroll= $(this).scrollTop();
+    var wScroll = $(this).scrollTop();
     $('.jumbotron img').css({
-        'transform':'translate(0px, '+ wScroll/6 +'%)'
+        'transform': 'translate(0px, ' + wScroll / 6 + '%)'
     });
     $('.jumbotron h1').css({
-        'transform':'translate(0px, '+ wScroll/2 +'%)'
+        'transform': 'translate(0px, ' + wScroll / 2 + '%)'
     });
     $('.jumbotron p').css({
-        'transform':'translate(0px, '+ wScroll/1 +'%)'
+        'transform': 'translate(0px, ' + wScroll / 1 + '%)'
     });
 
     //portofolio
-    if(wScroll>$('.portfolio').offset().top -250 & wScroll<$('.contact').offset().top){
-        $('.portfolio .img-thumbnail').each(function(i){
-            setTimeout(function(){
+    if (wScroll > $('.portfolio').offset().top - 250 & wScroll < $('.contact').offset().top) {
+        $('.portfolio .img-thumbnail').each(function (i) {
+            setTimeout(function () {
                 $('.portfolio .img-thumbnail').eq(i).addClass('active');
-            },500* (i))
-        }) 
+            }, 500 * (i))
+        })
     }
 
 
 });
-    // //portfolio
-    // $()
-
